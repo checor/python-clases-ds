a72b4dd7f7c9ddd761769e8cefd4f17705f08cf8
Sergio Urbina <checor@gmail.com>
2020-05-14 18:33:36 -0500

    Update hoteles_txt.py

--- sesion04/reto01/hoteles_txt.py
+++ sesion04/reto01/hoteles_txt.py
@@ -1,14 +1,14 @@
 cont  = True
 
 while cont:
-    nombre = input("Inserte nombre del hotel: ")
-    ubicacion = input("Inserte ubicación: ")
-    precio = input("Inserte precio por noche: ")
-    with open("hoteles.txt", 'a') as hoteles_file:
-        hoteles_file.write("{}\t{}\t{}\n".format(nombre, ubicacion, precio))
+    nombre = input("Inserte nombre del lugar: ")
+    latitud = input("Inserte latitud: ")
+    longitud = input("Inserte longitud: ")
+    with open("hoteles.txt", 'a') as estaciones_file:
+        estaciones_file.write("{}\t{}\t{}\n".format(nombre, latitud, longitud))
     valid = False
     while not valid:
-        c = input("Agregar otro hotel (s/N)? ")
+        c = input("Agregar otro lugar? (s/N)? ")
         c = c.lower()
         if c.startswith('s'):
             cont = True
